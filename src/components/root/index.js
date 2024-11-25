de7f60518fbf4f091bdc5e25c4ad97254cd48365
Jacob Matthews <jacmatthews7@gmail.com>
2024-11-25 10:32:08 +0000

    Added tsx file for components/root/index

--- src/components/root/index.js
+++ src/components/root/index.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import Navigation from '../navigation';
-// import Outlet
-import { Outlet } from 'react-router-dom';
-
-const Root = () => {
-    return (
-        <>
-            <Navigation/>
-            <Outlet/>
-        </>
-    );
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
 };
-
-export default Root;
+Object.defineProperty(exports, "__esModule", { value: true });
+var react_1 = __importDefault(require("react"));
+var navigation_1 = __importDefault(require("../navigation"));
+// import Outlet
+var react_router_dom_1 = require("react-router-dom");
+var Root = function () {
+    return (react_1.default.createElement(react_1.default.Fragment, null,
+        react_1.default.createElement(navigation_1.default, null),
+        react_1.default.createElement(react_router_dom_1.Outlet, null)));
+};
+exports.default = Root;
