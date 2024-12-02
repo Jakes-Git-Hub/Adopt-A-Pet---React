17281beb2b18979339cd23cf137c351061e7de2e
Jacob Matthews <jacmatthews7@gmail.com>
2024-12-02 10:28:20 +0000

    Fixed bug in handlers.tsx

--- src/mocks/handlers.tsx
+++ src/mocks/handlers.tsx
@@ -3,6 +3,85 @@ import types from './data/types.json';
 import animals from './data/animals.json';
 import details from './data/details.json';
 
+interface AnimalDetails {
+  id: number;
+  organization_id: string;
+  url: string;
+  type: string;
+  species: string;
+  breeds: {
+    primary: string;
+    secondary: string | null;
+    mixed: boolean;
+    unknown: boolean;
+  };
+  colors: {
+    primary: string | null;
+    secondary: string | null;
+    tertiary: string | null;
+  };
+  age: string;
+  gender: string;
+  size: string;
+  coat: string | null;
+  attributes: {
+    spayed_neutered: boolean;
+    house_trained: boolean;
+    declawed: boolean | null;
+    special_needs: boolean;
+    shots_current: boolean;
+  };
+  environment: {
+    children: boolean | null;
+    dogs: boolean | null;
+    cats: boolean | null;
+  };
+  tags: string[];
+  name: string;
+  description: string | null;
+  organization_animal_id: string | null;
+  photos: {
+    small: string;
+    medium: string;
+    large: string;
+    full: string;
+  }[];
+  primary_photo_cropped: {
+    small: string;
+    medium: string;
+    large: string;
+    full: string;
+  } | null;
+  videos: {
+    embed: string;
+  }[];
+  status: string;
+  status_changed_at: string;
+  published_at: string;
+  distance: number | null;
+  contact: {
+    email: string;
+    phone: string | null;
+    address: {
+      address1: string | null;
+      address2: string | null;
+      city: string;
+      state: string;
+      postcode: string;
+      country: string;
+    };
+  };
+  _links: {
+    self: { href: string };
+    type: { href: string };
+    organization: { href: string };
+  };
+}
+
+interface Details {
+  [key: string]: AnimalDetails;
+}
+
 export const handlers = [
   rest.get('/types', (_req, res, ctx) => {
     return res(ctx.status(200), ctx.json(types));
@@ -30,7 +109,7 @@ export const handlers = [
   }),
   rest.get('/animals/:id', (req, res, ctx) => {
     const { id } = req.params as { id: string };
-    let response = details[id];
+    let response = (details as Details)[id];
     if (!response) {
       return res(ctx.status(404));
     }
